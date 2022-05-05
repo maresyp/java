cfc83e35fb4b40695d0753b64b7a38b0cda37da9
maresyp <mattq.2000@icloud.com>
2022-05-05 19:44:21 +0200

    main

--- src/com/maresyp/Main.java
+++ src/com/maresyp/Main.java
@@ -1,8 +1,29 @@
 package com.maresyp;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        while (true) {
+            System.out.println("Wybierz operacje: ");
+            System.out.println("[1] Wczytaj poprzedni plik");
+            System.out.println("[2] Zacznij od nowa");
+            System.out.println("[3] Wyjscie");
+            try (Scanner scanner = new Scanner(System.in)) {
+                switch (scanner.nextInt()) {
+                    case 1:
+                        System.out.println("1");
+                        break;
+                    case 2:
+                        System.out.println("2");
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        System.out.println("Twoj wybor nie jest prawidlowy");
+                }
+            }
+        }
     }
 }
