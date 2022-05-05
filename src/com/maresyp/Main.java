c4eca22648ae923e7ce655c276862da270674158
maresyp <mattq.2000@icloud.com>
2022-05-05 20:43:19 +0200

    implement LoadFile

--- src/com/maresyp/Main.java
+++ src/com/maresyp/Main.java
@@ -1,5 +1,10 @@
 package com.maresyp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -13,11 +18,12 @@ public class Main {
             try (Scanner scanner = new Scanner(System.in)) {
                 switch (scanner.nextInt()) {
                     case 1:
-                        System.out.println("1");
-                        break;
+                        Map<String, ArrayList<String>> groceries = LoadFile();
+                        System.out.println(groceries);
+                        return;
                     case 2:
                         System.out.println("2");
-                        break;
+                        return;
                     case 3:
                         return;
                     default:
@@ -25,5 +31,29 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static Map<String, ArrayList<String>> LoadFile() {
+        File file = new File("./src/com/maresyp/zakupy.txt");
+        HashMap<String, ArrayList<String>> groceries = new HashMap<>();
+        try (Scanner scanner = new Scanner(file)) {
+            String lastCategory = "";
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("[") && line.endsWith("]")) {
+                    groceries.put(line, new ArrayList<>());
+                    lastCategory = line;
+                } else if (!line.isEmpty()){
+                    groceries.get(lastCategory).add(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return groceries;
+    }
+
+    public static void SaveFile() {
+
     }
 }
